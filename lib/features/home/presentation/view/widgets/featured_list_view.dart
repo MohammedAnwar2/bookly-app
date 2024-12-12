8db35c83eb573cf2a9683af65da3f9c67dfcfbb3
mohammed anwar <moanbm123@gmail.com>
2024-12-12 11:10:02 +0530

    chnage  FeaturedBookListView and FeaturedBookListViewItem widgets names

--- lib/features/home/presentation/view/widgets/featured_list_view.dart
+++ lib/features/home/presentation/view/widgets/featured_list_view.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_app/features/home/presentation/view/widgets/featured_book_list_view_item.dart';
+import 'package:bookly_app/features/home/presentation/view/widgets/featured_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedBookListView extends StatelessWidget {
