66fe12324d87166fd0f5a94c9996df31c0bb8e31
mohammed anwar <moanbm123@gmail.com>
2024-12-13 08:22:09 +0530

    Add GestureDetector to BestSellerListViewItem and create CustomHomeViewDetails widget
    Updated BestSellerListViewItem to include a GestureDetector for navigation to the home view details. Also, created a new widget, CustomHomeViewDetails, which contains a row of icons for closing and adding to cart functionalities.

--- lib/features/home/presentation/view/widgets/best_seller_list_view_items.dart
+++ lib/features/home/presentation/view/widgets/best_seller_list_view_items.dart
@@ -1,7 +1,8 @@
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/app_style.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
 import 'book_rating.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
@@ -11,47 +12,53 @@ class BestSellerListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        SizedBox(
-          height: 125,
-          child: AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: CachedNetworkImage(
-                fit: BoxFit.fill,
-                imageUrl:
-                    "https://t4.ftcdn.net/jpg/00/05/86/25/240_F_5862533_wQ6IJRVm6vLtub3aqirHc0AsUK3EfloS.jpg",
-                placeholder: (context, url) =>
-                    const CircularProgressIndicator(),
-                errorWidget: (context, url, error) => const Icon(Icons.error),
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.homeViewDetails);
+      },
+      child: Row(
+        children: [
+          SizedBox(
+            height: 125,
+            child: AspectRatio(
+              aspectRatio: 2.5 / 4,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: CachedNetworkImage(
+                  fit: BoxFit.fill,
+                  imageUrl:
+                      "https://t4.ftcdn.net/jpg/00/05/86/25/240_F_5862533_wQ6IJRVm6vLtub3aqirHc0AsUK3EfloS.jpg",
+                  placeholder: (context, url) =>
+                      const CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                ),
               ),
             ),
           ),
-        ),
-        const SizedBox(width: 16),
-        Expanded(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Text("Harry Potter and the Goblet of Fire",
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: AppStyles.styleRegular20(context).copyWith(height: 1.2)),
-            const SizedBox(height: 3),
-            Text("J.K Rowing",
-                style: AppStyles.styleRegular20(context)
-                    .copyWith(color: Colors.white.withOpacity(0.8))),
-            const SizedBox(height: 3),
-            const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("19.99\$"),
-                  BookRating(),
-                ])
-          ]),
-        ),
-      ],
+          const SizedBox(width: 16),
+          Expanded(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text("Harry Potter and the Goblet of Fire",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style:
+                      AppStyles.styleRegular20(context).copyWith(height: 1.2)),
+              const SizedBox(height: 3),
+              Text("J.K Rowing",
+                  style: AppStyles.styleRegular20(context)
+                      .copyWith(color: Colors.white.withOpacity(0.8))),
+              const SizedBox(height: 3),
+              const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("19.99\$"),
+                    BookRating(),
+                  ])
+            ]),
+          ),
+        ],
+      ),
     );
   }
 }
