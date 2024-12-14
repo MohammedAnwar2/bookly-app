6858754455fcd18a275cc6772e3080e062221ec7
mohammed anwar <moanbm123@gmail.com>
2024-12-14 12:55:13 +0530

    build HomeLocalDataSource class with methods

--- lib/features/home/data/data_source/home_local_data_source.dart
+++ lib/features/home/data/data_source/home_local_data_source.dart
@@ -1,19 +1,19 @@
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 
 abstract class HomeLocalDataSource {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  List<BookEntity> fetchFeaturedBooks();
+  List<BookEntity> fetchNewestBooks();
 }
 
 class HomeRemoteDataSourceImp extends HomeLocalDataSource {
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() {
+  List<BookEntity> fetchFeaturedBooks() {
     // TODO: implement fetchFeaturedBooks
     throw UnimplementedError();
   }
 
   @override
-  Future<List<BookEntity>> fetchNewestBooks() {
+  List<BookEntity> fetchNewestBooks() {
     // TODO: implement fetchNewestBooks
     throw UnimplementedError();
   }
