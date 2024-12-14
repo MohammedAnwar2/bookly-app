5b0912572979963ba45aa661552365c17a4a973f
mohammed anwar <moanbm123@gmail.com>
2024-12-14 14:29:38 +0530

    Implement fetchFeaturedBooks method in HomeRepoImp

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,13 +1,29 @@
 import 'package:bookly_app/core/class/either_class.dart';
 import 'package:bookly_app/core/error/error.dart';
+import 'package:bookly_app/features/home/data/data_source/home_local_data_source.dart';
+import 'package:bookly_app/features/home/data/data_source/home_remote_data_source.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/features/home/domain/repo/home_repo.dart';
 
 class HomeRepoImp extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+  HomeRepoImp(
+      {required this.homeRemoteDataSource, required this.homeLocalDataSource});
+
   @override
-  Future<Either<Fiauler, List<BookEntity>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Fiauler, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      List<BookEntity> booksList;
+      booksList = homeLocalDataSource.fetchFeaturedBooks();
+      if (booksList.isNotEmpty) {
+        return Either.right(booksList);
+      }
+      booksList = await homeRemoteDataSource.fetchFeaturedBooks();
+      return Either.right(booksList);
+    } catch (e) {
+      return Either.left(Fiauler());
+    }
   }
 
   @override
