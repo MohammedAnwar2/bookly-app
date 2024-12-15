219962fea654308a57bc1e454e7e7f04a7454710
mohammed anwar <moanbm123@gmail.com>
2024-12-15 16:25:52 +0530

    Add `toString` method to state classes for better debugging

--- lib/features/home/presentation/manager/featured_book_cubit/featured_book_state.dart
+++ lib/features/home/presentation/manager/featured_book_cubit/featured_book_state.dart
@@ -3,16 +3,26 @@ part of 'featured_book_cubit.dart';
 @immutable
 sealed class FeaturedBookState {}
 
-final class FeaturedBookInitial extends FeaturedBookState {}
+final class FeaturedBookInitial extends FeaturedBookState {
+  @override
+  String toString() => 'Initial State';
+}
 
-final class FeaturedBookLoading extends FeaturedBookState {}
+final class FeaturedBookLoading extends FeaturedBookState {
+  @override
+  String toString() => 'Loading State';
+}
 
 final class FeaturedBookSuccess extends FeaturedBookState {
   final List<BookEntity> bookList;
   FeaturedBookSuccess(this.bookList);
+  @override
+  String toString() => 'Success State';
 }
 
 final class FeaturedBookFailure extends FeaturedBookState {
   final String errorMessage;
   FeaturedBookFailure(this.errorMessage);
+  @override
+  String toString() => 'Failure State';
 }
--- lib/features/home/presentation/manager/newest_book_cubit/newest_book_state.dart
+++ lib/features/home/presentation/manager/newest_book_cubit/newest_book_state.dart
@@ -3,18 +3,29 @@ part of 'newest_book_cubit.dart';
 @immutable
 sealed class NewestBookState {}
 
-final class NewestBookInitial extends NewestBookState {}
+final class NewestBookInitial extends NewestBookState {
+  @override
+  String toString() => 'Initial State';
+}
 
-final class NewestBookLoading extends NewestBookState {}
+final class NewestBookLoading extends NewestBookState {
+  @override
+  String toString() => 'Loading State';
+}
 
 final class NewestBookSuccess extends NewestBookState {
   final List<BookEntity> booksList;
 
   NewestBookSuccess(this.booksList);
+  @override
+  String toString() => 'Success State';
 }
 
 final class NewestBookFailure extends NewestBookState {
   final String errorMessage;
 
   NewestBookFailure(this.errorMessage);
+
+  @override
+  String toString() => 'Failure State';
 }
