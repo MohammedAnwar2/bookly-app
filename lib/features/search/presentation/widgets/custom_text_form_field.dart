0041880f2000c5dd10ae55498f25a2a4d3a819f1
mohammed anwar <moanbm123@gmail.com>
2024-12-18 17:34:43 +0530

    Add CustomTextFormField widget

---
+++ lib/features/search/presentation/widgets/custom_text_form_field.dart
@@ -0,0 +1,30 @@
+import 'package:bookly_app/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: 0.7,
+      child: TextFormField(
+        decoration: InputDecoration(
+            enabledBorder: buildOutlineInputBorder(),
+            focusedBorder: buildOutlineInputBorder(),
+            errorBorder: buildOutlineInputBorder(),
+            hintText: "search",
+            hintStyle: TextStyle(color: AppColors.white),
+            suffixIcon: const Icon(FontAwesomeIcons.magnifyingGlass, size: 22)),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildOutlineInputBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: BorderSide(color: AppColors.white),
+    );
+  }
+}
