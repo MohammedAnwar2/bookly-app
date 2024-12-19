b001dfa60f589ced0217b1ffcda1a06afe320f77
mohammed anwar <moanbm123@gmail.com>
2024-12-19 08:28:00 +0530

    Update SearchViewBody to conditionally display search results or another books list

--- lib/features/search/presentation/widgets/search_view_body.dart
+++ lib/features/search/presentation/widgets/search_view_body.dart
@@ -1,29 +1,49 @@
 import 'package:bookly_app/core/utils/app_style.dart';
+import 'package:bookly_app/features/search/presentation/manager/search_cubit/search_cubit.dart';
 import 'package:bookly_app/features/search/presentation/widgets/another_items_search_list_view_bloc_consumer.dart';
 import 'package:bookly_app/features/search/presentation/widgets/custom_text_form_field.dart';
+import 'package:bookly_app/features/search/presentation/widgets/search_list_view_bloc_consumer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomTextFormField(
-            onChanged: (text) {},
+    return BlocSelector<SearchCubit, SearchState, bool>(
+      selector: (state) {
+        return context.read<SearchCubit>().isTyping;
+      },
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomTextFormField(
+                onChanged: (text) {},
+              ),
+              SizedBox(height: 10),
+              if (!state) ...{
+                Text("Another Books",
+                    style: AppStyles.styleSemiBolde18(context)),
+                SizedBox(height: 20),
+                Expanded(
+                  child: AnotherItemSearchListViewBlocConsumer(),
+                )
+              } else ...{
+                Text("result search",
+                    style: AppStyles.styleSemiBolde18(context)),
+                SizedBox(height: 20),
+                Expanded(
+                  child: SearchListViewBlocConsumer(),
+                )
+              }
+            ],
           ),
-          SizedBox(height: 10),
-          Text("Another Books", style: AppStyles.styleSemiBolde18(context)),
-          SizedBox(height: 20),
-          Expanded(
-            child: AnotherItemSearchListViewBlocConsumer(),
-          )
-        ],
-      ),
+        );
+      },
     );
   }
 }
