fc492c143e9c340d9b5eb4626fa680702fc57888
mohammed anwar <moanbm123@gmail.com>
2024-12-20 19:29:27 +0530

    Refactor SearchViewBody to use TextInputCubit and implement debouncing for search input

--- lib/features/search/presentation/widgets/search_view_body.dart
+++ lib/features/search/presentation/widgets/search_view_body.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'package:bookly_app/core/utils/app_style.dart';
 import 'package:bookly_app/features/search/presentation/manager/search_cubit/search_cubit.dart';
+import 'package:bookly_app/features/search/presentation/manager/text_input_cubit/text_input_cubit.dart';
 import 'package:bookly_app/features/search/presentation/widgets/another_items_search_list_view_bloc_builder.dart';
 import 'package:bookly_app/features/search/presentation/widgets/custom_text_form_field.dart';
 import 'package:bookly_app/features/search/presentation/widgets/search_list_view_bloc_consumer.dart';
@@ -7,59 +9,91 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SearchViewBody extends StatelessWidget {
+class SearchViewBody extends StatefulWidget {
   const SearchViewBody({super.key});
+  @override
+  State<SearchViewBody> createState() => _SearchViewBodyState();
+}
+
+class _SearchViewBodyState extends State<SearchViewBody> {
+  Timer? debounce;
+  @override
+  void dispose() {
+    debounce?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BlocSelector<SearchCubit, SearchState, bool>(
-      selector: (state) {
-        return context.read<SearchCubit>().isTyping;
-      },
-      builder: (context, state) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CustomTextFormField(
-                // controller: TextEditingController(),
-                controller: context.read<SearchCubit>().controller,
+    final searchCubit = context.read<SearchCubit>();
+    final textInputCubit = context.read<TextInputCubit>();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          BlocSelector<TextInputCubit, TextInputState, bool>(
+            selector: (state) => state is TextInputTyping,
+            builder: (context, isTyping) {
+              return CustomTextFormField(
+                controller: textInputCubit.controller,
                 suffixIcon: IconButton(
-                    onPressed: () {
-                      if (state) {
-                        context.read<SearchCubit>().onDelete();
-                      }
-                    },
-                    icon: Icon(
-                        state
-                            ? FontAwesomeIcons.xmark
-                            : FontAwesomeIcons.magnifyingGlass,
-                        size: 22)),
-                onChanged: (text) {
-                  context.read<SearchCubit>().onSearch(text);
-                },
-              ),
-              SizedBox(height: 10),
-              if (!state) ...{
-                Text("Another Books",
-                    style: AppStyles.styleSemiBolde18(context)),
-                SizedBox(height: 20),
-                Expanded(
-                  child: AnotherItemSearchListViewBlocBuilder(),
+                  onPressed: () {
+                    if (isTyping) {
+                      textInputCubit.clearInput();
+                    }
+                  },
+                  icon: Icon(
+                    isTyping
+                        ? FontAwesomeIcons.xmark
+                        : FontAwesomeIcons.magnifyingGlass,
+                    size: 22,
+                  ),
                 ),
-              } else ...{
-                Text("result search",
-                    style: AppStyles.styleSemiBolde18(context)),
-                SizedBox(height: 20),
-                Expanded(
-                  child: SearchListViewBlocConsumer(),
-                )
-              }
-            ],
+                onChanged: (text) {
+                  textInputCubit.onTyping(text);
+                  _debouncedFetchBooks(text, searchCubit);
+                },
+              );
+            },
           ),
-        );
-      },
+          SizedBox(height: 10),
+          BlocSelector<TextInputCubit, TextInputState, bool>(
+            selector: (state) => state is TextInputTyping,
+            builder: (context, isTyping) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    isTyping ? "Result Search" : "Another Books",
+                    style: AppStyles.styleSemiBolde18(context),
+                  ),
+                  SizedBox(height: 20),
+                  Expanded(
+                    child: isTyping
+                        ? SearchListViewBlocConsumer()
+                        : AnotherItemSearchListViewBlocBuilder(),
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
+      ),
     );
+  }
+
+  void _debouncedFetchBooks(String text, SearchCubit searchCubit) {
+    if (text.isNotEmpty) {
+      if (debounce?.isActive ?? false) {
+        debounce?.cancel();
+      }
+      debounce = Timer(
+        Duration(milliseconds: 200),
+        () {
+          searchCubit.fetchSpecificsBooks(title: text);
+        },
+      );
+    }
   }
 }
